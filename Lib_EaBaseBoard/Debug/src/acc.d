4e0883c4fa5689ace9036879fcfbf9404bbd5a42
oyfml <41881561+oyfml@users.noreply.github.com>
2018-07-30 10:17:53 +0800

    Add files via upload

---
+++ Lib_EaBaseBoard/Debug/src/acc.d
@@ -0,0 +1,19 @@
+src/acc.d: ../src/acc.c \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_MCU\inc/lpc17xx_i2c.h \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_MCU\inc/lpc_types.h \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_EaBaseBoard\inc/acc.h
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_MCU\inc/lpc17xx_i2c.h:
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_MCU\inc/lpc_types.h:
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_EaBaseBoard\inc/acc.h:
---
+++ Lib_EaBaseBoard/Debug/src/font5x7.d
@@ -0,0 +1,4 @@
+src/font5x7.d: ../src/font5x7.c \
+ C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_EaBaseBoard\inc/font_macro.h
+
+C:\Users\Lenovo\Documents\LPCXpresso_6.1.4_194\workspace2\Lib_EaBaseBoard\inc/font_macro.h:
